3c1afa67aea47dbcfd27a08cb22f26835a08b17e
Lincon <59184093+ItsLincon@users.noreply.github.com>
2023-06-29 05:12:43 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,8 +1,1 @@
-ESX = nil
 
-Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
-    end
-end)
